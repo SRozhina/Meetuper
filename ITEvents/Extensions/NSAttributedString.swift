01b6045d374a2ddd5bbb19a72eabfd9c9db0185a
Sofia <rsofico@gmail.com>
2018-03-02 17:32:25 +0300

    Add converting html description to nsattributed string

---
+++ ITEvents/Extensions/NSAttributedString.swift
@@ -0,0 +1,39 @@
+import Foundation
+import UIKit
+
+extension NSAttributedString {
+    convenience init(htmlString html: String, font: UIFont? = nil, useDocumentFontSize: Bool = true) throws {
+        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+            .documentType: NSAttributedString.DocumentType.html,
+            .characterEncoding: String.Encoding.utf8.rawValue
+        ]
+        
+        let data = html.data(using: .utf8, allowLossyConversion: true)
+        guard (data != nil),
+            let fontFamily = font?.familyName,
+            let attr = try? NSMutableAttributedString(data: data!, options: options, documentAttributes: nil) else {
+                try self.init(data: data ?? Data(html.utf8), options: options, documentAttributes: nil)
+                return
+        }
+        
+        let fontSize: CGFloat? = useDocumentFontSize ? nil : font!.pointSize
+        let range = NSRange(location: 0, length: attr.length)
+        attr.enumerateAttribute(.font, in: range, options: .longestEffectiveRangeNotRequired) { attrib, range, _ in
+            if let htmlFont = attrib as? UIFont {
+                let traits = htmlFont.fontDescriptor.symbolicTraits
+                var descrip = htmlFont.fontDescriptor.withFamily(fontFamily)
+                
+                if (traits.rawValue & UIFontDescriptorSymbolicTraits.traitBold.rawValue) != 0 {
+                    descrip = descrip.withSymbolicTraits(.traitBold)!
+                }
+                
+                if (traits.rawValue & UIFontDescriptorSymbolicTraits.traitItalic.rawValue) != 0 {
+                    descrip = descrip.withSymbolicTraits(.traitItalic)!
+                }
+                
+                attr.addAttribute(.font, value: UIFont(descriptor: descrip, size: fontSize ?? htmlFont.pointSize), range: range)
+            }
+        }
+        self.init(attributedString: attr)
+    }
+}
--- ITEvents/Screens/Event view/EventDescription.swift
+++ ITEvents/Screens/Event view/EventDescription.swift
@@ -6,9 +6,10 @@ class EventDescription: DecorationView {
     
     class func initiateAndSetup(with description: String) -> EventDescription {
         let eventDescriptionView = UINib(nibName: "EventDescription", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! EventDescription
-        eventDescriptionView.descriptionLabel.text = description
+        let attributedString = try? NSAttributedString(htmlString: description,
+                                                       font: UIFont.systemFont(ofSize: 15),
+                                                       useDocumentFontSize: false)
+        eventDescriptionView.descriptionLabel.attributedText = attributedString ?? NSAttributedString(string: description)
         return eventDescriptionView
     }
-    
-    //TODO make text formatted from html https://stackoverflow.com/questions/37048759/swift-display-html-data-in-a-label-or-textview
 }
