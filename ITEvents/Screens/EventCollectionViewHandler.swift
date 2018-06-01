582bbe31124673481935975ebfe6d85e7bf89593
Sofia <rsofico@gmail.com>
2018-06-01 20:55:21 +0300

    Move IEventCollectionViewHandler to separate file

--- ITEvents/Screens/EventCollectionViewHandler.swift
+++ ITEvents/Screens/EventCollectionViewHandler.swift
@@ -104,16 +104,3 @@ class EventCollectionViewHandler: NSObject, IEventCollectionViewHandler {
         collectionView.reloadData()
     }
 }
-
-protocol IEventCollectionViewHandler: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
-    var isListLayoutSelected: Bool { get }
-    
-    var isLoadingIndicatorShown: Bool { get }
-    
-    func setEvents(_ newEvents: [EventCollectionCellViewModel])
-    
-    func showLoadingIndicator()
-    func hideLoadingIndicator()
-    
-    func toggleListLayout()
-}
---
+++ ITEvents/Screens/IEventCollectionViewHandler.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+protocol IEventCollectionViewHandler: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    var isListLayoutSelected: Bool { get }
+    
+    var isLoadingIndicatorShown: Bool { get }
+    
+    func setEvents(_ newEvents: [EventCollectionCellViewModel])
+    
+    func showLoadingIndicator()
+    func hideLoadingIndicator()
+    
+    func toggleListLayout()
+}
