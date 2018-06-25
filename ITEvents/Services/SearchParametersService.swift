5145e5638d6e2ec5b775786dd862f63612a93cf7
Sofia <rsofico@gmail.com>
2018-06-25 22:55:17 +0300

    Move sort logic from Service to Presenter

--- ITEvents/Services/SearchParametersService.swift
+++ ITEvents/Services/SearchParametersService.swift
@@ -1,12 +1,4 @@
 class SearchParametersService: ISearchParametersService {
-    var selectedTags: [Tag] = [] {
-        didSet {
-            selectedTags = selectedTags.sorted { $0.name < $1.name }
-        }
-    }
-    var otherTags: [Tag] = [] {
-        didSet {
-            otherTags = otherTags.sorted { $0.name < $1.name }
-        }
-    }
+    var selectedTags: [Tag] = []
+    var otherTags: [Tag] = []
 }
