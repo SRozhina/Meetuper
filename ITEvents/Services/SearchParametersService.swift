6d73ee9aed4c7f26442b4f361b68993f09e896e7
Sofia <rsofico@gmail.com>
2018-06-25 16:10:13 +0300

    Implement tags sorting

--- ITEvents/Services/SearchParametersService.swift
+++ ITEvents/Services/SearchParametersService.swift
@@ -1,4 +1,12 @@
 class SearchParametersService: ISearchParametersService {
-    var selectedTags: [Tag] = []
-    var otherTags: [Tag] = []
+    var selectedTags: [Tag] = [] {
+        didSet {
+            selectedTags = selectedTags.sorted { $0.name < $1.name }
+        }
+    }
+    var otherTags: [Tag] = [] {
+        didSet {
+            otherTags = otherTags.sorted { $0.name < $1.name }
+        }
+    }
 }
