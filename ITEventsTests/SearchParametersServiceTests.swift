ed99343b843b170de46a7e9be34219eb9d00e053
Sofia <rsofico@gmail.com>
2018-07-01 22:59:26 +0300

    Change tests

--- ITEventsTests/SearchParametersServiceTests.swift
+++ ITEventsTests/SearchParametersServiceTests.swift
@@ -2,19 +2,28 @@ import XCTest
 @testable import ITEvents
 
 class SearchParametersServiceTests: XCTestCase {
-    let selectedTags = [Tag(id: 1, name: "JavaScript"),
-                        Tag(id: 2, name: "iOS")]
-    let otherTags = [Tag(id: 3, name: "Android"),
-                     Tag(id: 4, name: "Python")]
+    var searchParametersService: SearchParametersService!
     
-    override func setUp() { }
+    override func setUp() {
+        super.setUp()
+        searchParametersService = SearchParametersService()
 
-    override func tearDown() { }
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        searchParametersService = nil
+    }
 
     func testSearchParametersService() {
-        let searchParametersService = SearchParametersService()
+        let selectedTags = [Tag(id: 1, name: "JavaScript"),
+                            Tag(id: 2, name: "iOS")]
+        let otherTags = [Tag(id: 3, name: "Android"),
+                         Tag(id: 4, name: "Python")]
+        
         searchParametersService.selectedTags = selectedTags
         searchParametersService.otherTags = otherTags
+        
         XCTAssertEqual(searchParametersService.selectedTags, selectedTags)
         XCTAssertEqual(searchParametersService.otherTags, otherTags)
     }
--- ITEventsTests/SelectedEventServiceTests.swift
+++ ITEventsTests/SelectedEventServiceTests.swift
@@ -2,27 +2,35 @@ import XCTest
 @testable import ITEvents
 
 class SelectedEventServiceTests: XCTestCase {
-    let event = Event(id: 1,
-                      title: "Test event",
-                      dateInterval: DateInterval(start: Date(timeIntervalSinceNow: 60*60*24) ,
-                                                 end: Date(timeIntervalSinceNow: 60*60*27)),
-                      address: "Большой Сампсониевский проспект 28 к2 литД",
-                      city: "Санкт-Петербург",
-                      country: "Россия",
-                      description: "Description for event",
-                      tags: [Tag(id: 1, name: "JavaScript")],
-                      image: UIImage(named: "js")!,
-                      similarEventsCount: 0,
-                      source: EventSource(id: 1, name: "Timepad"),
-                      url: URL(string: "https://pitercss.timepad.ru/event/457262/"))
+    var selectedEventService: SelectedEventService!
     
-    override func setUp() { }
+    override func setUp() {
+        super.setUp()
+        selectedEventService = SelectedEventService()
+    }
 
-    override func tearDown() { }
+    override func tearDown() {
+        super.tearDown()
+        selectedEventService = nil
+    }
 
-    func testSavinSelectedEvent() {
-        let selectedEventService = SelectedEventService()
+    func testSavingSelectedEvent() {
+        let event = Event(id: 1,
+                          title: "Test event",
+                          dateInterval: DateInterval(start: Date(timeIntervalSinceNow: 60*60*24) ,
+                                                     end: Date(timeIntervalSinceNow: 60*60*27)),
+                          address: "Большой Сампсониевский проспект 28 к2 литД",
+                          city: "Санкт-Петербург",
+                          country: "Россия",
+                          description: "Description for event",
+                          tags: [Tag(id: 1, name: "JavaScript")],
+                          image: UIImage(named: "js")!,
+                          similarEventsCount: 0,
+                          source: EventSource(id: 1, name: "Timepad"),
+                          url: URL(string: "https://pitercss.timepad.ru/event/457262/"))
+        
         selectedEventService.selectedEvent = event
+        
         XCTAssertNotNil(selectedEventService.selectedEvent)
         XCTAssertEqual(selectedEventService.selectedEvent, event)
     }
