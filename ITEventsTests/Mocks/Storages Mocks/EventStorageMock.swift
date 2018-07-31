51eb925b9d9eb681b2b861b09262b69559ab3f4a
Sofia <rsofico@gmail.com>
2018-07-31 15:12:43 +0300

    Fix PR Comments

--- ITEventsTests/Mocks/Storages Mocks/EventStorageMock.swift
+++ ITEventsTests/Mocks/Storages Mocks/EventStorageMock.swift
@@ -2,14 +2,9 @@
 import Promises
 
 class EventStorageMock: IEventsStorage {
-    var searchEventsMocked: ((Range<Int>) -> Cancelable<EventsResult>)?
+    var searchEventsMocked: ((Range<Int>) -> Cancelable<EventsResult>)!
     
     func searchEvents(indexRange: Range<Int>, searchText: String, searchTags: [Tag]) -> Cancelable<EventsResult> {
-        if let searchEventsMocked = searchEventsMocked {
-            return searchEventsMocked(indexRange)
-        }
-        let eventsResult = EventsResult(events: [], totalEventsCount: 0)
-        let promise = Promise(eventsResult)
-        return Cancelable<EventsResult>(promise: promise)
+        return searchEventsMocked(indexRange)
     }
 }
--- ITEventsTests/SearchEventsPresenterTests/SearchEventsPresenterTests.swift
+++ ITEventsTests/SearchEventsPresenterTests/SearchEventsPresenterTests.swift
@@ -59,7 +59,7 @@ class SearchEventsPresenterTests: XCTestCase {
     func testSearchPresenterLoadMoreEvents() {
         //Given
         let allEvents = createTestEvents()
-        eventStorageMock.searchEventsMocked = { self.makeSearchButchEventsAction(indexRange: $0, events: allEvents) }
+        eventStorageMock.searchEventsMocked = { self.makeSearchBatchEventsAction(indexRange: $0, events: allEvents) }
         presenter.setup()
         XCTAssert(waitForPromises(timeout: 2))
         
@@ -77,7 +77,7 @@ class SearchEventsPresenterTests: XCTestCase {
     func testSearchPresenterAllEventsLoaded() {
         //Given
         let allEvents = createTestEvents()
-        eventStorageMock.searchEventsMocked = { self.makeSearchButchEventsAction(indexRange: $0, events: allEvents) }
+        eventStorageMock.searchEventsMocked = { self.makeSearchBatchEventsAction(indexRange: $0, events: allEvents) }
         presenter.setup()
         XCTAssert(waitForPromises(timeout: 2))
         
@@ -175,6 +175,7 @@ class SearchEventsPresenterTests: XCTestCase {
     
     func testSearchPresenterGetSearchParameters() {
         //Given
+        eventStorageMock.searchEventsMocked = { _ in self.makeEmptyResult() }
         presenter.setup()
         XCTAssert(waitForPromises(timeout: 2))
         
@@ -190,6 +191,7 @@ class SearchEventsPresenterTests: XCTestCase {
     
     func testSearchPresenterActivate() {
         //Given
+        eventStorageMock.searchEventsMocked = { _ in self.makeEmptyResult() }
         presenter.setup()
         
         //When
@@ -202,30 +204,17 @@ class SearchEventsPresenterTests: XCTestCase {
     
     func testSearchPresenterSelectEvent() {
         //Given
-        let event = Event(id: 1,
-                          title: "Test event1",
-                          dateInterval: DateInterval(start: Date(timeIntervalSinceNow: 60 * 60 * 24),
-                                                     end: Date(timeIntervalSinceNow: 60 * 60 * 27)),
-                          address: "Большой Сампсониевский проспект 28 к2 лит Д",
-                          city: "Санкт-Петербург",
-                          country: "Россия",
-                          description: "Description for event",
-                          tags: [Tag(id: 1, name: "JavaScript")],
-                          image: UIImage(named: "js")!,
-                          similarEventsCount: 2,
-                          source: EventSource(id: 1, name: "Timepad"),
-                          url: URL(string: "https://pitercss.timepad.ru/event/457262/"))
-        let allEvents = Array(repeating: event, count: 5)
+        let allEvents = createTestEvents()
         eventStorageMock.searchEventsMocked = { _ in self.makeEventResult(with: allEvents) }
         presenter.setup()
         XCTAssert(waitForPromises(timeout: 2))
 
         //When
-        presenter.selectEvent(with: 1)
+        presenter.selectEvent(with: allEvents[0].id)
         
         //Then
         XCTAssert(waitForPromises(timeout: 2))
-        XCTAssertEqual(selectedEventServiceMock.selectedEvent, event)
+        XCTAssertEqual(selectedEventServiceMock.selectedEvent, allEvents[0])
     }
     
     private func createTestTags() -> [Tag] {
@@ -244,49 +233,55 @@ class SearchEventsPresenterTests: XCTestCase {
     
     private func createTestEvents() -> [Event] {
         var events: [Event] = []
-        let event1 = Event(id: 1,
-                           title: "Test event1",
-                           dateInterval: DateInterval(start: Date(timeIntervalSinceNow: 60 * 60 * 24),
-                                                      end: Date(timeIntervalSinceNow: 60 * 60 * 27)),
-                           address: "Большой Сампсониевский проспект 28 к2 лит Д",
-                           city: "Санкт-Петербург",
-                           country: "Россия",
-                           description: "Description for event",
-                           tags: [Tag(id: 1, name: "JavaScript")],
-                           image: UIImage(named: "js")!,
-                           similarEventsCount: 2,
-                           source: EventSource(id: 1, name: "Timepad"),
-                           url: URL(string: "https://pitercss.timepad.ru/event/457262/"))
-        let event2 = Event(id: 2,
-                           title: "Test event2",
-                           dateInterval: DateInterval(start: Date(timeIntervalSinceNow: 60 * 60 * 24),
-                                                      end: Date(timeIntervalSinceNow: 60 * 60 * 27)),
-                           address: "Большой Сампсониевский проспект 28 к2 лит Д",
-                           city: "Санкт-Петербург",
-                           country: "Россия",
-                           description: "Description for event CSS",
-                           tags: [Tag(id: 1, name: "JavaScript"), Tag(id: 2, name: "CSS")],
-                           image: UIImage(named: "js")!,
-                           similarEventsCount: 2,
-                           source: EventSource(id: 1, name: "Timepad"),
-                           url: URL(string: "https://pitercss.timepad.ru/event/457262/"))
-        for _ in Array(1..<10) {
+        for index in Array(1..<10) {
+            let event1 = Event(id: index * 2,
+                               title: "Test event1",
+                               dateInterval: DateInterval(start: Date(timeIntervalSinceNow: 60 * 60 * 24),
+                                                          end: Date(timeIntervalSinceNow: 60 * 60 * 27)),
+                               address: "Большой Сампсониевский проспект 28 к2 лит Д",
+                               city: "Санкт-Петербург",
+                               country: "Россия",
+                               description: "Description for event",
+                               tags: [Tag(id: 1, name: "JavaScript")],
+                               image: UIImage(named: "js")!,
+                               similarEventsCount: 2,
+                               source: EventSource(id: 1, name: "Timepad"),
+                               url: URL(string: "https://pitercss.timepad.ru/event/457262/"))
+            let event2 = Event(id: index * 2 + 1,
+                               title: "Test event2",
+                               dateInterval: DateInterval(start: Date(timeIntervalSinceNow: 60 * 60 * 24),
+                                                          end: Date(timeIntervalSinceNow: 60 * 60 * 27)),
+                               address: "Большой Сампсониевский проспект 28 к2 лит Д",
+                               city: "Санкт-Петербург",
+                               country: "Россия",
+                               description: "Description for event CSS",
+                               tags: [Tag(id: 1, name: "JavaScript"), Tag(id: 2, name: "CSS")],
+                               image: UIImage(named: "js")!,
+                               similarEventsCount: 2,
+                               source: EventSource(id: 1, name: "Timepad"),
+                               url: URL(string: "https://pitercss.timepad.ru/event/457262/"))
             events.append(contentsOf: [event1, event2])
         }
         return events
     }
     
-    private func makeSearchButchEventsAction(indexRange: Range<Int>, events: [Event]) -> Cancelable<EventsResult> {
+    private func makeSearchBatchEventsAction(indexRange: Range<Int>, events: [Event]) -> Cancelable<EventsResult> {
         let updatedIndexRange = events.count < indexRange.upperBound
             ? indexRange.lowerBound..<events.count
             : indexRange
-        let butchEvents = Array(events[updatedIndexRange])
-        return makeEventResult(with: butchEvents, totalEventsCount: events.count)
+        let batchEvents = Array(events[updatedIndexRange])
+        return makeEventResult(with: batchEvents, totalEventsCount: events.count)
     }
     
     private func makeEventResult(with events: [Event], totalEventsCount: Int = 0) -> Cancelable<EventsResult> {
         let count = totalEventsCount == 0 ? events.count : totalEventsCount
         let eventsResult = EventsResult(events: events, totalEventsCount: count)
+        let promise = Promise(eventsResult)
+        return Cancelable<EventsResult>(promise: promise)
+    }
+    
+    private func makeEmptyResult() -> Cancelable<EventsResult> {
+        let eventsResult = EventsResult(events: [], totalEventsCount: 0)
         let promise = Promise(eventsResult)
         return Cancelable<EventsResult>(promise: promise)
     }
